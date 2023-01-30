a11912b0ec84a2898c8a56396af75a750d559fe3
Matias Baldanza <matiasbaldanza@gmail.com>
2023-01-30 12:04:38 -0300

    test: added wrapper for fetch and fetch pokemons by name and id

--- index.js
+++ index.js
@@ -7,8 +7,18 @@ const pokeImgElement = document.querySelector('[poke-img]');
 
 const requestURL = new URL('https://pokeapi.co/api/v2/');
 
-async function getPokemon(name) {
-    requestURL.pathname += `pokemon/${name}`;
+async function getPokemonByName(name) {
+    const pokemon = await fetchPokemon(name);
+    return pokemon;
+}
+
+async function getPokemonByID(id) {
+    const pokemon = await fetchPokemon(String(id));
+    return pokemon
+}
+
+async function fetchPokemon(queryParams) {
+    requestURL.pathname += `pokemon/${queryParams}`;
     let data = ''; 
     try {
         const promise = await fetch(requestURL);
@@ -23,9 +33,12 @@ function displayPokemon(pokemon) {
     pokeNameElement.textContent = pokemon.forms[0].name;
     pokeImgElement.setAttribute('src', 
                                 pokemon.sprites.other['official-artwork'].front_default);
+    pokeImgElement.setAttribute('alt', pokemon.forms[0].name);
+    pokeImgElement.setAttribute('title', pokemon.forms[0].name);
 } 
 
-const result = getPokemon('pikachu');
+// const result = getPokemonByName('pikachu');
+const result = getPokemonByID('1');
 result.then((pokemonData) => displayPokemon(pokemonData));
 
 
