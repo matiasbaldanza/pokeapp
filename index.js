4f03b8d08fa05a23dae96a604e2cb438cc48414d
Matias Baldanza <matiasbaldanza@gmail.com>
2023-02-02 11:46:42 -0300

    temp: attempt to do pagination became impossibly difficult due to prior bad technical decisions. Will reimplement fetch wrapper using full URL instead of the URL javascript object

--- index.js
+++ index.js
@@ -33,42 +33,53 @@ const nextButton = document.querySelector('[btn-next]');
 const requestURL = new URL('https://pokeapi.co/');
 const prefixURL = 'api/v2/';
 
+// GLOBALS
+let currentPokemonOffset;
+
 const lastPokemonIndex = async function() { } 
 
 // event listeners
 searchForm.addEventListener('submit', handleFormSubmit );
 // prevButton.addEventListener('click', prevPokemon );
-// nextButton.addEventListener('click', nextPokemon );
+nextButton.addEventListener('click', nextPokemon );
 
 async function handleFormSubmit(event) {
     event.preventDefault();
+    const form = Object.fromEntries(new FormData(event.target));
+    getAndDisplayPokemon(form.pokemon.toLowerCase());
+}
+
+function setCurrentPokemon(pokemon) {
+    currentPokemonIndex = pokemon.id;
+}
+
+async function nextPokemon() {
+    // https://pokeapi.co/api/v2/pokemon/?offset=1007&limit=1
+    // averiguar cual es la pagina donde sale este pokemon
+    const currentPokemonPage = await fetchPage('pokemon', )
+    
+    // averiguar cual es la proxima pagina 
+    // averiguar cuál es el próximo pokemon de la pagina del proximo
+
+    
+/*     const nextPage = await fetchPage('pokemon', currentPokemonIndex, 1);
+    const nextPokemonName = nextPage.results[0].name;
+
+    getAndDisplayPokemon(nextPokemonName); */
+}
+
+async function getAndDisplayPokemon(name) {
+    notifyUser('Buscando...', 'toast-info');
     clearResults();
     clearNotifications();
-
-    const form = Object.fromEntries(new FormData(event.target));
-    const pokemon = await getPokemonByName(form.pokemon.toLowerCase());
     
+    const pokemon = await fetchFromAPI('pokemon', name);
+    setCurrentPokemon(pokemon);
+
     if (pokemon) {
         clearNotifications();
         displayPokemon(pokemon);    
     }
-}
-
-function nextPokemon() {
-    // https://pokeapi.co/api/v2/pokemon/?offset=1007&limit=1
-
-    /* .next
-        results[n].url // el actual en esta página
-
-        if .next === null // No hay próximo
-        if .previous === null // No hay previo
-        .previous = query de la pagina anterior  */
-}
-
-async function getPokemonByName(name) {
-    notifyUser('Buscando...', 'toast-info');
-    const pokemon = await fetchFromAPI('pokemon', name);
-    return pokemon;
 }
 
 function displayPokemon(pokemon) {
@@ -116,6 +127,20 @@ async function fetchFromAPI(resource, queryParams) {
         return data;
     } catch(error) {
         handleError(error, queryParams);
+    }
+}
+
+async function fetchPage(resource, offset, limit) {
+    requestURL.pathname = `${prefixURL}${resource}`;
+    requestURL.searchParams.append('offset', offset);
+    requestURL.searchParams.append('limit', limit);
+    let data = ''; 
+    try {
+        const response = await myFetch(requestURL);
+        data = await response.json();
+        return data;
+    } catch(error) {
+        handleError(error, '');
     }
 }
 
